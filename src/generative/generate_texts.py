7861569a44c8c5adffe3146184c2d381ba8e12d5
Vered Shwartz <vered1986@gmail.com>
2021-07-29 13:57:57 -0700

    Generate simile zero-shot

--- src/generative/generate_texts.py
+++ src/generative/generate_texts.py
@@ -157,7 +157,6 @@ def generate_conditional(tokenizer, model, args, input, device):
         num_return_sequences=max(1, args.beams)
     ).tolist()
 
-    preds = [pred[:pred.index(tokenizer.eos_token_id)] if tokenizer.eos_token_id in pred else pred for pred in preds]
     preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
     preds = [pred.split(".")[0] for pred in preds]
     return preds
--- src/generative/generate_texts.sh
+++ src/generative/generate_texts.sh
@@ -8,13 +8,63 @@ declare -a lm_models=(gpt2-xl)
 declare -a knowledge_types=(context literal)
 
 
-for knowledge_type in "${knowledge_types[@]}"
+for lm in "${lm_models[@]}"
 do
-    python -m src.generative.generate_texts_inferences \
-        --in_file data/${task}_inferences_${knowledge_type}/${set}.jsonl \
-        --out_file output/${task}/generative/gpt2-xl_${knowledge_type}_${set}_predictions_k5.jsonl \
-        --model_name_or_path output/${task}/generative/gpt2-xl_${knowledge_type} \
+    python -m src.generative.generate_texts \
+            --in_file data/${task}/${set}.jsonl \
+            --out_file output/${task}/generative/${lm}_${set}_predictions_k5.jsonl \
+            --model_name_or_path output/${task}/generative/${lm} \
+            --k 5 \
+            --temperature 0.7 \
+            --device ${device};
+
+    for knowledge_type in "${knowledge_types[@]}"
+    do
+        python -m src.generative.generate_texts_inferences \
+            --in_file data/${task}_inferences_${knowledge_type}/${set}.jsonl \
+            --out_file output/${task}/generative/${lm}_${knowledge_type}_${set}_predictions_k5.jsonl \
+            --model_name_or_path output/${task}/generative/${lm}_${knowledge_type} \
+            --k 5 \
+            --temperature 0.7 \
+            --device ${device};
+    done
+done
+
+for lm in "${enc_dec_models[@]}"
+do
+    python -m src.generative.generate_texts \
+        --in_file data/${task}/${set}.jsonl \
+        --out_file output/${task}/generative/${lm/facebook\//}_${set}_predictions_k5.jsonl \
+        --model_name_or_path output/${task}/generative/${lm/facebook\//} \
         --k 5 \
         --temperature 0.7 \
+        --is_encoder_decoder \
+        --device ${device};
+done
+
+# Zero-shot
+
+for lm in "${lm_models[@]}"
+do
+    python -m src.generative.generate_texts \
+            --in_file data/${task}/${set}.jsonl \
+            --out_file output/${task}/generative/${lm}_zeroshot_${set}_predictions_k5.jsonl \
+            --model_name_or_path ${lm} \
+            --k 5 \
+            --temperature 0.7 \
+            --is_zero_shot \
+            --device ${device};
+done
+
+for lm in "${enc_dec_models[@]}"
+do
+    python -m src.generative.generate_texts \
+        --in_file data/${task}/${set}.jsonl \
+        --out_file output/${task}/generative/${lm/facebook\//}_zeroshot_${set}_predictions_k5.jsonl \
+        --model_name_or_path ${lm} \
+        --k 5 \
+        --temperature 0.7 \
+        --is_encoder_decoder \
+        --is_zero_shot \
         --device ${device};
 done
