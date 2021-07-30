dad244a9ee093900880d21b7fbeee6a5abd7e640
Vered Shwartz <vered1986@gmail.com>
2021-07-30 13:40:10 -0700

    Generative zero-shot and few shot
    After simile data update

--- src/generative/gpt3_few_shot.sh
+++ src/generative/gpt3_few_shot.sh
@@ -2,9 +2,10 @@
 
 api_key=$1
 set=$2
+task=$3
 
 python -m src.generative.gpt3_few_shot \
         --openai_api_key ${api_key} \
-        --train_file data/idioms/train.jsonl \
-        --eval_file data/idioms/${set}.jsonl \
-        --out_prediction_file output/generative/gpt3_fewshot_${set}_predictions_p0.9.jsonl;
+        --train_file data/${task}/train.jsonl \
+        --eval_file data/${task}/${set}.jsonl \
+        --out_prediction_file output/${task}/generative/gpt3_fewshot_${set}_predictions_p0.9.jsonl;
