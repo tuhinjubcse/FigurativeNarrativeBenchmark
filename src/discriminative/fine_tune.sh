4ea3ecddb2103e75d60e8eb9fd0bb628322f69de
Tuhin Chakrabarty <tuhinjubcse@gmail.com>
2022-01-07 21:21:49 -0500

    Update fine_tune.sh

--- src/discriminative/fine_tune.sh
+++ src/discriminative/fine_tune.sh
@@ -5,6 +5,25 @@ task=$2 # idiom / simile
 lm="roberta-large"
 declare -a knowledge_types=(literal context)
 
+#baseline
+CUDA_VISIBLE_DEVICES=${device} python -m src.discriminative.run_multiple_choice \
+        --data_dir test/${task}/ \
+        --output_dir output/${task}/discriminative/ \
+        --task_name ${task} \
+        --model_name_or_path ${lm} \
+        --do_eval \
+        --overwrite_output_dir \
+        --gradient_accumulation_steps 1 \
+        --evaluation_strategy steps \
+        --overwrite_cache \
+        --num_train_epochs 10 \
+        --logging_steps 388 \
+        --save_steps 388 \
+        --eval_steps 388 \
+        --per_device_train_batch_size 8 \
+        --per_device_eval_batch_size 8 \
+        --learning_rate 1e-5 ;
+
 for knowledge_type in "${knowledge_types[@]}"
 do
     CUDA_VISIBLE_DEVICES=${device} python -m src.discriminative.run_multiple_choice \
